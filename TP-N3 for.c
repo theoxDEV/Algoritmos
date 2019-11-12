2436df91876b081d7333a5bc1b8649a5275a4629
theoxDEV <56891192+theoxDEV@users.noreply.github.com>
2019-11-12 00:47:46 -0400

    Update TP-N3 for.c

--- TP-N3 for.c
+++ TP-N3 for.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
 
+int validacao_INPUT(int input_usuario){
+    printf("Informe o multiplicando: ");
+    scanf("%d", &input_usuario);
+        while(input_usuario < 1 || input_usuario > 10){
+            printf("Valor inválido, informe outro valor: ");
+            scanf("%d", &input_usuario);
+    }
+    return input_usuario;
+}
+
 int main()
 {
     int multiplicador = 1;
     int input_usuario;
     int produto = 0;
+    int input;
     
-    printf("Informe o multiplicando: ");
-    scanf("%d", &input_usuario);
+    input = validacao_INPUT(input_usuario);
     
     for(multiplicador; multiplicador <= 10; multiplicador++){
-        produto = input_usuario * multiplicador;
-        printf("Tabuada de %d: %d X %d = %d\n", input_usuario, input_usuario, multiplicador, produto);
+        produto = input * multiplicador;
+        printf("Tabuada de %d: %d X %d = %d\n", input, input, multiplicador, produto);
     }
-    
     return 0;
 }
